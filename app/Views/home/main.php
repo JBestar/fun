c464d636fa9540e4a48f2830a0f1124fa1d9148e
jbestar <jbestar1105@gmail.com>
2023-07-20 13:04:45 +0900

    20230720

--- app/Views/home/main.php
+++ app/Views/home/main.php
@@ -67,7 +67,7 @@
             <script type="text/javascript" src="/js/lib.js?ver=1"></script>
             <script type="text/javascript" src="/js/common.js?ver=2"></script>
             <script type="text/javascript" src="/js/SLB.js?ver=4"></script>
-            <script type="text/javascript" src="/js/main.js?ver=4"></script>
+            <script type="text/javascript" src="/js/main.js?ver=5"></script>
             <link rel="stylesheet" type="text/css" href="/css/devel.css?v=3" />
         <?php else : ?>
             <script type="text/javascript" src="/js/vue.js"></script>
